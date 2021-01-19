dcdb4ca2ed0997ce42d93b7096c1445f2e065ad2
Bagusth15 <bagustri15@gmail.com>
2021-01-19 20:02:18 +0700

    update routesnavigation

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const morgan = require("morgan")
-const cors = require("cors")
-const routerNavigation = require("./src")
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const routerNavigation = require("./src/routesNavigation");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan("dev"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
 
-app.use("/", routerNavigation)
+app.use("/", routerNavigation);
 
 app.listen(3000, () => {
-  console.log("Listening on Port 3000")
-})
+  console.log("Listening on Port 3000");
+});
