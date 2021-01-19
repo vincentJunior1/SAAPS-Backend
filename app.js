c556ecfba80a8465600dfeb53d96201401f3cc0a
Bagusth15 <bagustri15@gmail.com>
2021-01-19 19:59:25 +0700

    routing express

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const cors = require("cors")
+const routerNavigation = require("./src")
+
+const app = express()
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan("dev"))
+
+app.use("/", routerNavigation)
+
+app.listen(3000, () => {
+  console.log("Listening on Port 3000")
+})
